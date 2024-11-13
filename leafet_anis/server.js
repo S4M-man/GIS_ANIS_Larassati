e0ebba5cb8d3894499a17f5ff394b5e147077067
S4M <136122291+S4M-man@users.noreply.github.com>
2024-11-13 11:45:02 +0700

    Add files via upload

---
+++ leafet_anis/server.js
@@ -0,0 +1,23 @@
+// server.js
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+
+  // Listen for messages from clients
+  ws.on('message', (message) => {
+    console.log(`Received message: ${message}`);
+
+    // Broadcast the message to all connected clients
+    wss.clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+
+  ws.on('close', () => {
+    console.log('Client disconnected');
+  });
+});
